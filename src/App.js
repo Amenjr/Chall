07d9b45e73327f1f4bc8a73db3ab91f773677ca5
Amenjr <92280223+Amenjr@users.noreply.github.com>
2021-11-23 16:45:47 +0100

    chall

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import react from 'react';
 import './App.css';
+class App extends react.Component{
+  constructor(){
+    super();
+    this.state={todos:[{id:0,task:"eat",done:false},{id:1,task:"sleep",done:false}],newText:""};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  }
+  
+  handleAdd=()=>{
+
+this.setState({todos: [...this.state.todos,{id:Math.random(),task:this.state.newText,done:false}] , 
+
+newText:""
+})
+  }
+  delete=(id)=>{
+    this.setState({todos:this.state.todos.filter(el=> el.id !==id)})
+  }
+  don=(id)=>{
+    
+    this.setState({todos:this.state.todos.map(el=> el.id===id ?{...el,done:!el.done}:el ) })
+    
+  }
+
+
+  render(){
+    console.log(this.state.todos)
+    return(
+      <div>
+        {
+        this.state.todos.map(el=>
+        <div>
+      
+        <li   style={{textDecoration: el.done? "line-through" : null}}  >{el.task}
+        <span> :  </span>
+        <button onClick={()=>this.don(el.id)}>done</button>
+        <button onClick={()=>this.delete(el.id)}>remove</button>
+        </li>
+        
+        </div>)
+        }
+        <input type="text" value={this.state.newText} onChange={(e)=>this.setState({newText:e.target.value})}></input>
+        <button   onClick={this.handleAdd} >add</button>
+        
+</div>
+    );
+  }
 }
 
 export default App;
